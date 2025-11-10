1e3b649fcf17a460816b3d33d827ffe51c3d9df6
Thinhinane_AKRICHE <174352479+Thinhinane-AKRICHE@users.noreply.github.com>
2025-11-10 13:13:10 +0100

    create components "ContactList"

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ def get_contacts():
     json_contacts = list(map(lambda x: x.to_json(), contacts))
     return jsonify({"contacts": json_contacts})
 
-@app.route("/create_contact", method=["POST"])
+@app.route("/create_contact", methods=["POST"])
 def create_contact():
     first_name = request.json.get("firstName")
     last_name = request.json.get("lastName")
@@ -30,7 +30,33 @@ def create_contact():
     return jsonify({"message": "User created!"}), 201
 
 
+@app.route("/update_contact/<int:user_id>", methods=["PATCH"])
+def update_contact(user_id):
+    contact = Contact.query.get(user_id)
 
+    if not contact:
+        return jsonify({"message": "User not found"}), 404
+    
+    data = request.json
+    contact.first_name = data.get("firstName", contact.first_name)
+    contact.last_name = data.get("lastName", contact.last_name)
+    contact.email = data.get("email", contact.email)
+
+    db.session.commit()
+
+    return jsonify({"message": "User updated."}), 200
+    
+@app.route("/delete_contact/<int:user_id>", methods= ["DELETE"])
+def delete_contact(user_id):
+    contact = contact.query.get(user_id)
+
+    if not contact :
+        return jsonify({"message": "user not found"}), 404
+
+    db.session.delete(contact)
+    db.session.commit()
+
+    return jsonify({"message": "User deleted!"}), 200
 
 if __name__ == "__main__":
     with app.app_context():
